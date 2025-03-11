093715a80edf43b63602a4e97489fa07b26a361e
Prashanth Xavier <xavier211192@gmail.com>
2025-03-11 16:39:33 +0100

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,1 +1,21 @@
+from dataclasses import dataclass, field
+from typing import List, Optional
 
+
+@dataclass
+class TableProperties:
+    name: str
+    z_order: Optional[List[str]]=field(default_factory=list)
+    vacuum:bool = True
+    optimize:bool = True
+    vacuum_retention_hour:int = 168
+    optim_filter:Optional[str]=""
+
+dataset = TableProperties(name="ds1",vacuum=True,optimize=True,z_order:['a','b'])
+
+from delta.tables import *
+deltaTable = DeltaTable.forName(spark,dataset.name)
+if dataset.z_order:
+  deltaTable.optimize().executeZOrderBy(dataset.z_order)
+elif dataset.optim_filter:
+  deltaTable.optimize().where(dataset.optim_filter).executeCompaction()
