17368aac2f372d68cfb880489090754f62f431e5
Prashanth Xavier <xavier211192@gmail.com>
2025-03-29 16:10:46 +0100

    add uv

--- src/duckexplore.py
+++ src/duckexplore.py
@@ -1,11 +1,14 @@
 import duckdb
 
-duck_df = duckdb.read_parquet('data/flights.parquet')
-# duck_df.limit(10).show()
+def analyze_flights():
+    duck_df = duckdb.read_parquet('data/flights.parquet')
 
+    #Filter departure delays greater than 1000 minutes
+    duck_df.filter('dep_delay>1000').show()
 
-#Filter departure delays greater than 1000 minutes
-duck_df.filter('dep_delay>1000').show()
+    # Aggregate average delay per carrier airport
+    duck_df.select("carrier,dep_delay").aggregate(" carrier,avg(dep_delay)").show()
+    duck_df.select("dest, dep_delay").aggregate("dest, avg(dep_delay)").show()
 
-# Aggregate average delay per destination airport
-duck_df.select("carrier,dep_delay").aggregate(" carrier,avg(dep_delay)").show()
+analyze_flights()
+
